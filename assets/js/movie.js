534433cde0de324da694a5cf62a8392b2d558661
rachaelloh <lohrachael@hotmail.com>
2019-11-22 02:01:41 +0800

    basic linking of api to page and make search more dynamic.

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,3 +1,6 @@
+//CONSTANTS
+const APIKey = 'de007f66726e0db6dce2c74935c5dda5';
+const URL = 'https://api.themoviedb.org/3/search/movie?de007f66726e0db6dce2c74935c5dda5';
 
 //selecting elements from DOM
 const searchBtn = document.querySelector('#searchBtn');
@@ -7,5 +10,18 @@ const input = document.querySelector('#input');
 searchBtn.onclick = function(event) {
     event.preventDefault(); //prevent any default behavior that browser is doing such as refereshing page
     const value = input.value;
+
+    const newURL = URL + '&query' + 'value'; //to make it more dynamic so that it still works across
+
+
+    //built in js function fetch. pass in the url
+    fetch(newURL)
+        .then((res) => res.json()) //to return json format
+        .then((data) => {
+            console.log('Data: ', data);
+        })
+        .catch((error) => { //to catch errors or if api is down
+            console.log('error: ', error);
+        });
     console.log ('Value: ', value); //value- the input that users give, will appear in the console
 }
