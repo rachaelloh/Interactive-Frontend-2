13f8d2163b9c7e739e59deef919014d726667b94
rachaelloh <lohrachael@hotmail.com>
2019-11-22 01:27:23 +0800

    edited js to make the form work when users input a value with the search button.

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,2 +1,11 @@
 
-console.log('Hello World');
+//selecting elements from DOM
+const searchBtn = document.querySelector('#searchBtn');
+const input = document.querySelector('#input');
+
+
+searchBtn.onclick = function(event) {
+    event.preventDefault(); //prevent any default behavior that browser is doing such as refereshing page
+    const value = input.value;
+    console.log ('Value: ', value); //value- the input that users give, will appear in the console
+}
