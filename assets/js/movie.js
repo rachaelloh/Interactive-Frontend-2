84d516f1d8f0d795bc2b7c5f7a05507c6804b99e
rachaelloh <lohrachael@hotmail.com>
2019-11-23 12:17:37 +0800

    mainly to select element from html file and select movieblock value to place in html file.

--- assets/js/movie.js
+++ assets/js/movie.js
@@ -1,49 +1,65 @@
 //CONSTANTS
 const APIKey = 'de007f66726e0db6dce2c74935c5dda5';
-const URL = 'https://api.themoviedb.org/3/search/movie?api_key=de007f66726e0db6dce2c74935c5dda5';
+const URL = 'https://api.themoviedb.org/3/search/movie?api_key=';
+const image = 'https://image.tmdb.org/t/p/w500/kvpNZAQow5es1tSY6XW2jAZuPPG.jpg'
 
 //selecting elements from DOM
 const searchBtn = document.querySelector('#searchBtn');
 const input = document.querySelector('#input');
+const movieSearch = document.querySelector('#moviesSearch');
 
-function containMovie() {
+function movieSections(movies){
+   return movies.map((movie) => {
+    //to link to poster path of the api    
+    return `
+            <img src=${movie.poster_path} data-movie-id=${movie.id}/>
+        `;
+    })
+}
+
+function containMovie(movies) { //the div of the movie
     const movieElement = document.createElement('div');
     movieElement.setAttribute('class', 'movie');
 
-    const movieTemplate = `
+    //this section will be appended to the function containMovie
+    const movieTemplate = ` 
         <section class="section">
-            <img
-                src="https://image.tmdb.org/t/p/w500/rZd0y1X1Gw4t583f01Qzj8DYY66.jpg"
-                alt=""
-                date-movie-id="557"
-            />
-            <img
-                src="https://image.tmdb.org/t/p/w500/rjbNpRMoVvqHmhmksbokcyCr7wn.jpg"
-                alt=""
-                data-movie-id="429617"
-            />
+            ${movieSections(movies)}
         </section>
         <div class="content">
             <p id="content-close">X</p>
         </div>
     `;
+    
+    /*the above `` does not allow a child to append to it, 
+    hence the below will work*/
+    movieElement.innerHTML = movieTemplate;
+    return movieElement;
 }
 
 searchBtn.onclick = function(event) {
-    event.preventDefault(); //prevent any default behavior that browser is doing such as refereshing page
+    //prevent any default behavior that browser is doing such as refereshing page
+    event.preventDefault(); 
     const value = input.value;
 
-    const newURL = URL + '&query='+value; //to make it more dynamic so that it still works across
+    //to make it more dynamic so that it still works across
+    const newURL = URL + APIKey + '&query=' + value; 
 
 
     //built in js function fetch. pass in the url
     fetch(newURL)
         .then((res) => res.json()) //to return json format
         .then((data) => {
+            const movies = data.results;
+            const movieBlock = containMovie(movies);
+            /*select element "movieSearch from html file 
+            and js select the movieblock values to be placed into the html file"*/
+            movieSearch.appendChild(movieBlock); 
             console.log('Data: ', data);
         })
         .catch((error) => { //to catch errors or if api is down
             console.log('Error: ', error);
         });
-    console.log ('Value: ', value); //value- the input that users give, will appear in the console
+    //value- the input that users give, will appear in the console
+    console.log ('Value: ', value); 
 }
